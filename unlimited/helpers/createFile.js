0f8a0dda859d44700e1bbfb8341a8d22e12e21fa
steven daddario <daddarios@dupage.edu>
2021-10-15 23:19:50 -0500

    added filtering

--- unlimited/helpers/createFile.js
+++ unlimited/helpers/createFile.js
@@ -5,22 +5,22 @@ const aws = require('aws-sdk');
 
 aws.config.region = 'us-east-2';
 const s3 = new aws.S3({
-    'accessKeyId': process.env.S3_ACCESS_KEY_ID, 
+    'accessKeyId': process.env.S3_ACCESS_KEY_ID,
     'secretAccessKey': process.env.S3_SECRET_ACCESS_KEY
-}); 
+});
 
 
-function uploadFile(path){
-    fs.readFile(path, (err, data)=>{
+function uploadFile(path) {
+    fs.readFile(path, (err, data) => {
         const params = {
-            Key: 'Expirations.js', 
-            Bucket: process.env.S3_BUCKET, 
+            Key: 'Expirations.js',
+            Bucket: process.env.S3_BUCKET,
             Body: data
-        }; 
+        };
 
-        s3.upload(params,(err, info)=>{
-            if(err){
-                throw err; 
+        s3.upload(params, (err, info) => {
+            if (err) {
+                throw err;
             }
             console.log('success', info.Location);
         });
@@ -28,32 +28,81 @@ function uploadFile(path){
 }
 
 
-function createFile(path){
-    fs.readFile(path, 'utf-8', (err, data)=>{
-        if(err){
-            console.error(err); 
-            throw new Error(); 
+function isOutOfService(trl) {
+    trl = trl.slice(-4);
+    if (trl[2].toUpperCase().trim() === 'OUT OF SERVICE') {
+        return true;
+    }
+    else if (trl[3].trim().toUpperCase() === 'REQUIRED' && trl[1].trim().toUpperCase() === 'PRE') {
+        return true;
+    }
+    return false;
+}
+
+function filterTrailers(data) {
+    data.reduce((a, b, i) => {
+        if (i === 0 || !b || !b[0]) {
+            return a;
         }
-        data = data.split(/\r?\n/); 
-        while(data[0].split(',')[0].trim() !== 'Resource ID'){
-            data.shift(); 
+        b = b.split(',');
+        if (!isOutOfService(b)) {
+            return a;
         }
+        else if (b.toUpperCase().includes('UL')) {
+            b = b.slice(2);
+        }
+        else if (b.toUpperCase().includes('UCIQ')) {
+            b = b.slice(3);
+        }
+
+
+        a.push(b);
+        return a;
+    }, []);
+    return data;
+}
+
+function createFile(path) {
+    fs.readFile(path, 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+            throw new Error();
+        }
+
+        // array of rows delimted by the new line
+        data = data.split(/\r?\n/);
+
+        // get rid of every empty row before the header
+        while (data[0].split(',')[0].trim() !== 'Resource ID') {
+            data.shift();
+        }
+
+        // get rid of the header row
         data.shift();
-        let trailers = ''; 
-        for(let row of data){
-            trailers += row + '\n'; 
+
+        data = filterTrailers(data);
+        
+        let trailers = '';
+        for (let row of data) {
+            trailers += row + '\n';
         }
-        fs.writeFile(config.rootDir + '/static/unlimited/Expirations.js', "var expirations = `" + trailers  + "`;", 'utf-8', (err)=>{
-            if(err){
+
+
+        // write javascript code
+        fs.writeFile(config.rootDir + '/static/unlimited/Expirations.js', "var expirations = `" + trailers + "`;", 'utf-8', (err) => {
+            if (err) {
                 console.error(err);
-                throw new Error();  
+                throw new Error();
             }
             uploadFile(config.rootDir + '/static/unlimited/Expirations.js');
-        }); 
+        });
     });
 
 }
 
 
 
-module.exports = createFile; 
+
+
+
+module.exports = createFile;
